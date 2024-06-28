efa68ac8849fddf530b6658284702e98eaa89dee
Arimardan108 <mishraarimardan934@gmail.com>
2024-06-28 14:53:51 +0530

    Update file

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,12 +1,26 @@
 import { Component } from "react";
 import MovieCard from "./MovieCard";
 class MovieList extends Component {
+  constructor() {
+    super();
+    //Creating the state object
+    this.state = {
+      title: "The Avengers",
+      plot: "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+      rating: "8.0",
+      price: 199,
+      star: 0,
+      fav: false,
+      isInCart: false,
+    };
+  }
   render() {
+    const { title, poster, plot, price, rating, star, isIncart } = this.state;
     return (
       <>
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
+        <MovieCard movies={this.state} />
       </>
     );
   }
