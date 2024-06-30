bba1bbfedde715a587ab00c1b622ffe70a011f5f
Arimardan108 <mishraarimardan934@gmail.com>
2024-06-30 14:59:42 +0530

    Update file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
 import App from "./App";
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
