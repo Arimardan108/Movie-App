8ed5c575b93a5380f65a290a80d04c11d1734c91
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-01 14:16:43 +0530

    Update file

---
+++ src/MovieList.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import MovieCard from "./MovieCard";
+class MovieList extends Component {
+  render() {
+    return (
+      <>
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+      </>
+    );
+  }
+}
+export default MovieList;
