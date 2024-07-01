de6d3be232b6eed2bc5ffd3918e0332a78008194
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-01 14:50:09 +0530

    Update file

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,13 +1,29 @@
 import { Component } from "react";
 import MovieCard from "./MovieCard";
 class MovieList extends Component {
+  constructor() {
+    super();
+    //Creating the state object
+    this.state = {
+      title: "The Avengers",
+      plot: "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+      rating: "8.0",
+      price: 199,
+      star: 0,
+      fav: false,
+      isInCart: false,
+    };
+
+    //Binding the event handler in the constructor if event handler is not an arrow function
+    // this.addStars = this.addStars.bind(this)
+  }
   render() {
+    const { title, plot, price, rating, stars, fav, isIncart } = this.state;
     return (
       <>
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
+        <MovieCard movies={this.state} />
       </>
     );
   }
