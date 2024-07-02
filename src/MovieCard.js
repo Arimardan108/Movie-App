63d279d9cc4d9e8537d0e38fdbd9b5cc8f9dddf5
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-02 17:42:48 +0530

    Update file with a new feature

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -49,74 +49,72 @@ class MovieCard extends React.Component {
   };
 
   render() {
-    console.log(this.props);
     //Destructing the state object in render function
     const { title, plot, poster, price, rating, star, fav, isInCart } =
       this.props.movies;
+    console.log(this.props.movies);
 
     return (
-      <div className="main">
-        {/**Movie Card */}
-        <div className="movie-card">
-          {/**Left section of Movie Card */}
-          <div className="left">
-            <img alt="poster" src={poster} />
-          </div>
+      //Movie Card
+      <div className="movie-card">
+        {/**Left section of Movie Card */}
+        <div className="left">
+          <img alt="poster" src={poster} />
+        </div>
 
-          {/**Right section Movie Card */}
-          <div className="right">
-            {/**Title, plot, price of the movie */}
-            <div className="title">{title}</div>
-            <div className="plot">{plot}</div>
-            <div className="price">Rs. {price}</div>
+        {/**Right section Movie Card */}
+        <div className="right">
+          {/**Title, plot, price of the movie */}
+          <div className="title">{title}</div>
+          <div className="plot">{plot}</div>
+          <div className="price">Rs. {price}</div>
 
-            {/**Footer starts here with ratings, stars and buttons */}
-            <div className="footer">
-              <div className="rating">{rating}</div>
+          {/**Footer starts here with ratings, stars and buttons */}
+          <div className="footer">
+            <div className="rating">{rating}</div>
 
-              {/**Star image with increase and decrease buttons and star count */}
-              <div className="star-dis">
-                <img
-                  className="str-btn"
-                  alt="Decrease"
-                  src="https://cdn-icons-png.flaticon.com/128/2801/2801932.png"
-                  onClick={this.decStars}
-                />
-                <img
-                  className="stars"
-                  alt="stars"
-                  src="https://cdn-icons-png.flaticon.com/128/2107/2107957.png"
-                />
-                <img
-                  className="str-btn"
-                  alt="increase"
-                  src="https://cdn-icons-png.flaticon.com/128/2997/2997933.png"
-                  // No binding required as addStars() is an arrow function
-                  onClick={this.addStars}
-                />
-                <span className="starCount">{star}</span>
-              </div>
-
-              {/**conditional rendering on Favourite button */}
-              <button
-                className={fav ? "unfavourite-btn" : "favourite-btn"}
-                onClick={this.toggleFav}
-              >
-                {fav ? "Un-favourite" : "Favourite"}
-              </button>
-              {/**
-               * {fav? <button className="unfavourite-btn" onClick={this.toggleFav}> Un-favourite </button> :
-               *       <button className="favourite-btn" onClick={this.toggleFav}> favourite </button>}
-               */}
-
-              {/**Conditional Rendering on Add to Cart Button */}
-              <button
-                className={isInCart ? "unfavourite-btn" : "cart-btn"}
-                onClick={this.toggleCart}
-              >
-                {isInCart ? "Remove from Cart" : "Add to Cart"}
-              </button>
+            {/**Star image with increase and decrease buttons and star count */}
+            <div className="star-dis">
+              <img
+                className="str-btn"
+                alt="Decrease"
+                src="https://cdn-icons-png.flaticon.com/128/2801/2801932.png"
+                onClick={this.decStars}
+              />
+              <img
+                className="stars"
+                alt="stars"
+                src="https://cdn-icons-png.flaticon.com/128/2107/2107957.png"
+              />
+              <img
+                className="str-btn"
+                alt="increase"
+                src="https://cdn-icons-png.flaticon.com/128/2997/2997933.png"
+                // No binding required as addStars() is an arrow function
+                onClick={this.addStars}
+              />
+              <span className="starCount">{star}</span>
             </div>
+
+            {/**conditional rendering on Favourite button */}
+            <button
+              className={fav ? "unfavourite-btn" : "favourite-btn"}
+              onClick={this.toggleFav}
+            >
+              {fav ? "Un-favourite" : "Favourite"}
+            </button>
+            {/**
+             * {fav? <button className="unfavourite-btn" onClick={this.toggleFav}> Un-favourite </button> :
+             *       <button className="favourite-btn" onClick={this.toggleFav}> favourite </button>}
+             */}
+
+            {/**Conditional Rendering on Add to Cart Button */}
+            <button
+              className={isInCart ? "unfavourite-btn" : "cart-btn"}
+              onClick={this.toggleCart}
+            >
+              {isInCart ? "Remove from Cart" : "Add to Cart"}
+            </button>
           </div>
         </div>
       </div>
