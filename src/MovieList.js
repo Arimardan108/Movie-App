4760e5d108298f52dce6c14930fe0b401ae5a5a2
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-02 15:15:36 +0530

    Update file

--- src/MovieList.js
+++ src/MovieList.js
@@ -18,24 +18,24 @@ class MovieList extends Component {
           isInCart: false,
         },
         {
-          title: "The Avengers",
-          plot: "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
+          title: "Iron Man",
+          plot: "After being held captive an afghan cave , billioaire engineer Tony Stack create a unique weaponized suit of amor to fight evil.",
           poster:
             "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
-          rating: "8.0",
-          price: 199,
+          rating: "8.5",
+          price: 399,
           star: 0,
           fav: false,
           isInCart: false,
         },
 
         {
-          title: "The Avengers",
-          plot: "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
+          title: "The Dark Knight",
+          plot: "When the means known as the joker weaks havoc on the poeple of Gotham.Batman must accept on of the gretest psychological and physical tests of his ability to fight injustice.",
           poster:
             "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
-          rating: "8.0",
-          price: 199,
+          rating: "9.0",
+          price: 299,
           star: 0,
           fav: false,
           isInCart: false,
@@ -51,7 +51,9 @@ class MovieList extends Component {
     // const { title, plot, price, rating, stars, fav, isIncart,poster } = this.state.movies;
     return (
       <>
-        {movies.map((movie) => <MovieCard movies= {movies} />)}
+        {movies.map((movie) => (
+          <MovieCard movies={movie} />
+        ))}
       </>
     );
   }
