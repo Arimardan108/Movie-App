e583961c40bbd92c76bef350b663bcd22be38f49
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-02 17:42:34 +0530

    Update file with a new feature

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,6 +1,7 @@
-import { Component } from "react";
+import React from "react";
 import MovieCard from "./MovieCard";
-class MovieList extends Component {
+
+class MovieList extends React.Component {
   constructor() {
     super();
     //Creating the state object
@@ -12,6 +13,17 @@ class MovieList extends Component {
           poster:
             "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
           rating: "8.0",
+          price: 99,
+          star: 0,
+          fav: false,
+          isInCart: false,
+        },
+        {
+          title: "The Dark Knight",
+          plot: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+          poster:
+            "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+          rating: "9.0",
           price: 199,
           star: 0,
           fav: false,
@@ -19,43 +31,29 @@ class MovieList extends Component {
         },
         {
           title: "Iron Man",
-          plot: "After being held captive an afghan cave , billioaire engineer Tony Stack create a unique weaponized suit of amor to fight evil.",
+          plot: "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
           poster:
-            "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
-          rating: "8.5",
-          price: 99,
-          star: 0,
-          fav: false,
-          isInCart: false,
-        },
-
-        {
-          title: "The Dark Knight",
-          plot: "When the means known as the joker weaks havoc on the poeple of Gotham.Batman must accept on of the gretest psychological and physical tests of his ability to fight injustice.",
-          poster:
-            "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
-          rating: "9.0",
-          price: 299,
+            "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg",
+          rating: "7.9",
+          price: 139,
           star: 0,
           fav: false,
           isInCart: false,
         },
       ],
     };
-
-    //Binding the event handler in the constructor if event handler is not an arrow function
-    // this.addStars = this.addStars.bind(this)
   }
   render() {
     const { movies } = this.state;
-    // const { title, plot, price, rating, stars, fav, isIncart,poster } = this.state.movies;
+
     return (
-      <>
-        {movies.map((movie) => (
-          <MovieCard movies={movie} />
+      <div className="main">
+        {movies.map((movie, index) => (
+          <MovieCard movies={movie} key={index} />
         ))}
-      </>
+      </div>
     );
   }
 }
+
 export default MovieList;
