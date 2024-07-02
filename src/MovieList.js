a3152f48b7912bd06ba7e2196afc1339d70924a5
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-02 11:00:19 +0530

    Update file

--- src/MovieList.js
+++ src/MovieList.js
@@ -5,15 +5,42 @@ class MovieList extends Component {
     super();
     //Creating the state object
     this.state = {
-      title: "The Avengers",
-      plot: "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
-      poster:
-        "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
-      rating: "8.0",
-      price: 199,
-      star: 0,
-      fav: false,
-      isInCart: false,
+      movies: [
+        {
+          title: "The Avengers",
+          plot: "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
+          poster:
+            "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+          rating: "8.0",
+          price: 199,
+          star: 0,
+          fav: false,
+          isInCart: false,
+        },
+        {
+          title: "The Avengers",
+          plot: "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
+          poster:
+            "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+          rating: "8.0",
+          price: 199,
+          star: 0,
+          fav: false,
+          isInCart: false,
+        },
+
+        {
+          title: "The Avengers",
+          plot: "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
+          poster:
+            "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+          rating: "8.0",
+          price: 199,
+          star: 0,
+          fav: false,
+          isInCart: false,
+        },
+      ],
     };
 
     //Binding the event handler in the constructor if event handler is not an arrow function
