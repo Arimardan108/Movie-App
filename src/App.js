87b0ee6a66a29995389224ed6d8eec434ffcce3f
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-03 09:16:34 +0530

    Update file with Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import MovieList from "./MovieList";
-
+import Navbar from "./Navbar";
 export default function App() {
   return (
     <>
-      <h1>Movie App</h1>
+      <Navbar />
       <MovieList />
     </>
   );
