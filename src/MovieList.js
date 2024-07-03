88ee2dd68803b5483b13270e6b01f0ede4d2fac7
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-03 20:56:27 +0530

    Update file with a new feature

--- src/MovieList.js
+++ src/MovieList.js
@@ -8,6 +8,7 @@ class MovieList extends React.Component {
     this.state = {
       movies: [
         {
+          id: 1,
           title: "The Avengers",
           plot: "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
           poster:
@@ -19,6 +20,7 @@ class MovieList extends React.Component {
           isInCart: false,
         },
         {
+          id: 2,
           title: "The Dark Knight",
           plot: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
           poster:
@@ -30,6 +32,7 @@ class MovieList extends React.Component {
           isInCart: false,
         },
         {
+          id: 3,
           title: "Iron Man",
           plot: "After being held captive in an Afghan cave, billionaire engineer Tony starsk creates a unique weaponized suit of armor to fight evil.",
           poster:
