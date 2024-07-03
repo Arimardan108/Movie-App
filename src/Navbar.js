b4607b279cf63d33cdd8b8d27dc4077acfce82cb
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-04 03:07:05 +0530

    update navbar style

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,7 +6,11 @@ class Navbar extends React.Component {
         <div style={styles.nav}>
           <div style={styles.title}>Movie-App</div>
           <div style={styles.cartContainer}>
-            <img style={styles.cartIcon} src="" alt="Cart Icon" />
+            <img
+              style={styles.cartIcon}
+              src="https://cdn-icons-png.flaticon.com/128/3144/3144456.png"
+              alt="Cart Icon"
+            />
             <span style={styles.cartCount}>3</span>
           </div>
         </div>
@@ -23,6 +27,7 @@ const styles = {
     background: "red",
     display: "flex",
     justifyContent: " space-between",
+    position: "relative",
   },
   title: {
     fontSize: 38,
@@ -32,7 +37,21 @@ const styles = {
     textTransform: "uppercase",
     marginLeft: 20,
   },
-  cartContainer: {},
-  cartIcon: {},
-  cartCount: {},
+  cartContainer: {
+    position: "relative",
+    cursor: "pointer",
+  },
+  cartIcon: {
+    height: 48,
+    marginRight: 20,
+  },
+  cartCount: {
+    background: "orange",
+    borderRadius: "50%",
+    padding: "4px 8px",
+    position: "absolute",
+    right: 10,
+    top: -5,
+    fontSize: 12,
+  },
 };
