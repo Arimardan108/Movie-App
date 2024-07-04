0c768bda308995833b588313ed3efded5a234b02
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-04 11:37:18 +0530

    update file using styled-components

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,24 +4,49 @@ import styled from "styled-components";
 const Nav = styled.div`
   width: 100%;
   height: 70px;
-  background: blue;
+  background: linear-gradient(170deg, #1bc059, #0d47a1);
   display: flex;
   justify-content: space-between;
+`;
+
+const Title = styled.div`
+  font-size: 38;
+  color: #fff;
+  font-weight: 600;
+  font-family: Montserrat, sans-ser;
+  text-transform: uppercase;
+  margin-left: 20;
+`;
+const CartIconContainer = styled.div`
+  position: relative;
+  cursor: pointer;
+`;
+const CartImg = styled.img`
+  height: 48px;
+  margin-right: 20px;
+`;
+const CartCount = styled.div`
+  background-color: ${(props) => props.color};
+  border-radius: 50%;
+  padding: 4px 8px;
+  position: absolute;
+  right: 10px;
+  top: -5px;
+  font-size: 12px;
 `;
 class Navbar extends React.Component {
   render() {
     return (
       <>
         <Nav>
-          <div style={styles.title}>Movie-App</div>
-          <div style={styles.cartContainer}>
-            <img
-              style={styles.cartIcon}
+          <Title>Movie-App</Title>
+          <CartIconContainer>
+            <CartImg
               src="https://cdn-icons-png.flaticon.com/128/3144/3144456.png"
               alt="Cart Icon"
             />
-            <span style={styles.cartCount}>3</span>
-          </div>
+            <CartCount color="yellow">3</CartCount>
+          </CartIconContainer>
         </Nav>
       </>
     );
