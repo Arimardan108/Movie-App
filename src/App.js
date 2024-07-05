87ea8958065305298548968e922e99f7bdba7e2d
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-05 16:21:49 +0530

    Update file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,80 @@
+import React from "react";
 import MovieList from "./MovieList";
 import Navbar from "./Navbar";
-export default function App() {
-  return (
-    <>
-      <Navbar />
-      <MovieList />
-    </>
-  );
+import (movies) from "./moviesData"
+import "./styles.css";
+class App extends React.Component {
+  constructor() {
+    super();
+    //Creating the state object
+    this.state = {
+      movies: movies,
+      carCount: 0,
+    };
+  }
+
+  handleAddStars = (movie) => {
+    const { movies } = this.state;
+    const movieId = movies.indexOf(movie);
+
+    if (movies[movieId].stars < 5) {
+      movies[movieId].stars += 0.5;
+    }
+
+    this.setState({
+      movies,
+    });
+  };
+
+  handleDecStars = (movie) => {
+    const { movies } = this.state;
+    const movieId = movies.indexOf(movie);
+
+    if (movies[movieId].stars > 0) {
+      movies[movieId].stars -= 0.5;
+    }
+
+    this.setState({
+      movies,
+    });
+  };
+
+  handleToggleFav = (movie) => {
+    const { movies } = this.state;
+    const movieId = movies.indexOf(movie);
+
+    movies[movieId].fav = !movies[movieId].fav;
+
+    this.setState({
+      movies,
+    });
+  };
+
+  handleAddtocart = (movie) => {
+    const { movies } = this.state;
+    const movieId = movies.indexOf(movie);
+
+    movies[movieId].isInCart = !movies[movieId].isInCart;
+
+    this.setState({
+      movies,
+    });
+  };
+
+  render() {
+    const {movies}=this.state;
+    return (
+      <>
+        <Navbar />
+        <MovieList movies={movies}
+        addStars={this.handleAddStars}
+        decStars={this.handleDecStars}
+        toggleFav={this.handleToggleFav}
+        toggleCart={this.handleAddtocart}
+        />
+      </>
+    );
+  }
 }
+
+export default App;
