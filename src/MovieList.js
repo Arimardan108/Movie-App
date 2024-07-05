60b3ad92a69ed56604e21943c0db7250b219f365
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-05 16:34:35 +0530

    Update file

--- src/MovieList.js
+++ src/MovieList.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
-
+    console.log(this.props);
     return (
       <>
         {movies.map((movie, index) => (
