eb4bb3197b0d84a4d1ad156c8970b8b857ada315
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-07 11:01:14 +0530

    Update file

--- src/Navbar.js
+++ src/Navbar.js
@@ -10,26 +10,45 @@ const Nav = styled.div`
 `;
 
 const Title = styled.div`
-  font-size: 30;
+  font-size: 30px;
   color: #fff;
-  font-weight: 600;
+  font-weight: 600px;
   font-family: Montserrat, sans-serif;
   text-transform: uppercase;
-  margin-left: 20;
+  margin-left: 20px;
+`;
+
+const CartIcon = styled.img`
+  height: 48px;
+  margin-right: 20px;
+`;
+
+const CartContainer = styled.div`
+  position: relative;
+  cursor: pointer;
+`;
+
+const CartCount = styled.div`
+  background: orange;
+  border-radius: 50%;
+  padding: 4px 8px;
+  position: absolute;
+  right: 10px;
+  top: -5px;
+  fontsize: 12px;
 `;
 class Navbar extends Component {
   render() {
     return (
       <Nav>
         <Title>Movie-App</Title>
-        <div style={styles.cartContainer}>
-          <img
+        <CartContainer>
+          <CartIcon
             src="https://cdn-icons-png.flaticon.com/128/891/891462.png"
             alt="cart-icon"
-            style={styles.cartIcon}
           />
-          <span style={styles.cartCount}>3</span>
-        </div>
+          <CartCount>3</CartCount>
+        </CartContainer>
       </Nav>
     );
   }
