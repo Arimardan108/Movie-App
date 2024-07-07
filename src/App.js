24e7a81327c594aa2f627dcc1e29d55f9bf9867d
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-07 12:26:14 +0530

    Update file

--- src/App.js
+++ src/App.js
@@ -51,21 +51,28 @@ export default class App extends React.Component {
   };
 
   handleAddtocart = (movie) => {
-    const { movies } = this.state;
+    let { movies, cartCount } = this.state;
     const movieId = movies.indexOf(movie);
-
     movies[movieId].isInCart = !movies[movieId].isInCart;
+
+    if (movies[movieId].isInCart) {
+      cartCount = cartCount + 1;
+    } else {
+      cartCount -= 1;
+    }
 
     this.setState({
       movies,
+      cartCount,
     });
+    console.log(cartCount);
   };
 
   render() {
-    const { movies } = this.state;
+    const { movies, cartCount } = this.state;
     return (
       <>
-        <Navbar />
+        <Navbar cartCount={cartCount} />
         <MovieList
           movies={movies}
           onIncStars={this.handleAddStars}
