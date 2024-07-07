452d7389ce4d71c99fbbb6cff081bb4ec461b874
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-07 09:10:05 +0530

    Update file

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,25 +1,53 @@
-import React from "react";
-import styles from "./nav.module.css";
+import React, { Component } from "react";
 
-function Navbar(props) {
-  const { cartCount } = props;
-
-  return (
-    <>
-      <div className={styles.nav}>
-        <div className={styles.title}>MOVIE APP</div>
-
-        <div className={styles.cartIconContainer}>
+class Navbar extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          height: 70,
+          background: "#4267b2",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          position: "relative",
+        }}
+      >
+        <div
+          style={{
+            fontSize: 30,
+            color: "#fff",
+            fontWeight: 600,
+            fontFamily: '"Montserrat", sans-serif',
+            textTransform: "uppercase",
+            marginLeft: 20,
+          }}
+        >
+          Movie-App
+        </div>
+        <div style={{ position: "relative", cursor: "pointer" }}>
           <img
-            className={styles.cartIcon}
-            alt="Cart-Icon"
+            style={{ height: 48, marginRight: 20 }}
             src="https://cdn-icons-png.flaticon.com/128/891/891462.png"
+            alt="cart-icon"
           />
-          <span className={styles.cartCount}>{cartCount}</span>
+          <span
+            style={{
+              background: "orange",
+              borderRadius: "50%",
+              padding: "4px 8px",
+              position: "absolute",
+              right: 10,
+              top: -5,
+              fontSize: 12,
+            }}
+          >
+            3
+          </span>
         </div>
       </div>
-    </>
-  );
+    );
+  }
 }
 
 export default Navbar;
