cf69ca79a69376318c7256016bc78d9664b6c41c
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-07 11:05:01 +0530

    Update file

--- src/Navbar.js
+++ src/Navbar.js
@@ -56,39 +56,39 @@ class Navbar extends Component {
 
 export default Navbar;
 
-const styles = {
-  nav: {
-    width: "100%",
-    height: 70,
-    background: "#4267b2",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    position: "relative",
-  },
-  title: {
-    fontSize: 30,
-    color: "#fff",
-    fontWeight: 600,
-    fontFamily: '"Montserrat", sans-serif',
-    textTransform: "uppercase",
-    marginLeft: 20,
-  },
-  cartContainer: {
-    position: "relative",
-    cursor: "pointer",
-  },
-  cartIcon: {
-    height: 48,
-    marginRight: 20,
-  },
-  cartCount: {
-    background: "orange",
-    borderRadius: "50%",
-    padding: "4px 8px",
-    position: "absolute",
-    right: 10,
-    top: -5,
-    fontSize: 12,
-  },
-};
+// const styles = {
+//   nav: {
+//     width: "100%",
+//     height: 70,
+//     background: "#4267b2",
+//     display: "flex",
+//     justifyContent: "space-between",
+//     alignItems: "center",
+//     position: "relative",
+//   },
+//   title: {
+//     fontSize: 30,
+//     color: "#fff",
+//     fontWeight: 600,
+//     fontFamily: '"Montserrat", sans-serif',
+//     textTransform: "uppercase",
+//     marginLeft: 20,
+//   },
+//   cartContainer: {
+//     position: "relative",
+//     cursor: "pointer",
+//   },
+//   cartIcon: {
+//     height: 48,
+//     marginRight: 20,
+//   },
+//   cartCount: {
+//     background: "orange",
+//     borderRadius: "50%",
+//     padding: "4px 8px",
+//     position: "absolute",
+//     right: 10,
+//     top: -5,
+//     fontSize: 12,
+//   },
+// };
