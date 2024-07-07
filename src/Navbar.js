701f95cef5e2d636ca8e7791046254e463b3e239
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-07 09:46:53 +0530

    Update file

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,47 +3,15 @@ import React, { Component } from "react";
 class Navbar extends Component {
   render() {
     return (
-      <div
-        style={{
-          height: 70,
-          background: "#4267b2",
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          position: "relative",
-        }}
-      >
-        <div
-          style={{
-            fontSize: 30,
-            color: "#fff",
-            fontWeight: 600,
-            fontFamily: '"Montserrat", sans-serif',
-            textTransform: "uppercase",
-            marginLeft: 20,
-          }}
-        >
-          Movie-App
-        </div>
-        <div style={{ position: "relative", cursor: "pointer" }}>
+      <div style={styles.nav}>
+        <div style={styles.title}>Movie-App</div>
+        <div style={styles.cartContainer}>
           <img
-            style={{ height: 48, marginRight: 20 }}
             src="https://cdn-icons-png.flaticon.com/128/891/891462.png"
             alt="cart-icon"
+            style={styles.cartIcon}
           />
-          <span
-            style={{
-              background: "orange",
-              borderRadius: "50%",
-              padding: "4px 8px",
-              position: "absolute",
-              right: 10,
-              top: -5,
-              fontSize: 12,
-            }}
-          >
-            3
-          </span>
+          <span style={styles.cartCount}>3</span>
         </div>
       </div>
     );
@@ -51,3 +19,40 @@ class Navbar extends Component {
 }
 
 export default Navbar;
+
+const styles = {
+  nav: {
+    width: "100%",
+    height: 70,
+    background: "#4267b2",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    position: "relative",
+  },
+  title: {
+    fontSize: 30,
+    color: "#fff",
+    fontWeight: 600,
+    fontFamily: '"Montserrat", sans-serif',
+    textTransform: "uppercase",
+    marginLeft: 20,
+  },
+  cartContainer: {
+    position: "relative",
+    cursor: "pointer",
+  },
+  cartIcon: {
+    height: 48,
+    marginRight: 20,
+  },
+  cartCount: {
+    background: "orange",
+    borderRadius: "50%",
+    padding: "4px 8px",
+    position: "absolute",
+    right: 10,
+    top: -5,
+    fontSize: 12,
+  },
+};
