f272e1d0df455b4d0f6ac45a7df0bb43ba800bb2
Arimardan108 <mishraarimardan934@gmail.com>
2024-07-07 11:56:42 +0530

    Update file

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,65 +1,10 @@
 import React from "react";
 import MovieCard from "./MovieCard";
-import { movies } from "./moviesData";
 
 class MovieList extends React.Component {
-  constructor() {
-    super();
-    //Creating the state object
-    this.state = {
-      movies: movies,
-    };
-  }
-
-  handleAddStars = (movie) => {
-    const { movies } = this.state;
-    const movieId = movies.indexOf(movie);
-
-    if (movies[movieId].stars < 5) {
-      movies[movieId].stars += 0.5;
-    }
-
-    this.setState({
-      movies,
-    });
-  };
-
-  handleDecStars = (movie) => {
-    const { movies } = this.state;
-    const movieId = movies.indexOf(movie);
-
-    if (movies[movieId].stars > 0) {
-      movies[movieId].stars -= 0.5;
-    }
-
-    this.setState({
-      movies,
-    });
-  };
-
-  handleToggleFav = (movie) => {
-    const { movies } = this.state;
-    const movieId = movies.indexOf(movie);
-
-    movies[movieId].fav = !movies[movieId].fav;
-
-    this.setState({
-      movies,
-    });
-  };
-
-  handleAddtocart = (movie) => {
-    const { movies } = this.state;
-    const movieId = movies.indexOf(movie);
-
-    movies[movieId].isInCart = !movies[movieId].isInCart;
-
-    this.setState({
-      movies,
-    });
-  };
   render() {
-    const { movies } = this.state;
+    const { movies, onIncStars, onDecStars, onClickFav, onClickAddtocart } =
+      this.props;
 
     return (
       <div className="main">
@@ -67,10 +12,10 @@ class MovieList extends React.Component {
           <MovieCard
             movies={movie}
             key={index}
-            onIncStars={this.handleAddStars}
-            onDecStars={this.handleDecStars}
-            onClickFav={this.handleToggleFav}
-            onClickAddtocart={this.handleAddtocart}
+            onIncStars={onIncStars}
+            onDecStars={onDecStars}
+            onClickFav={onClickFav}
+            onClickAddtocart={onClickAddtocart}
           />
         ))}
       </div>
